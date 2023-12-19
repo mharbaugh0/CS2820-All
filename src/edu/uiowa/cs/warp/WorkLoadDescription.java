fb2c9cb1e3b463b5d35acfa023ea7268ab5a0724
Steve Goddard <steve-goddard@uiowa.edu>
2023-12-19 17:04:28 -0600

    Spring 2024 Codebase

---
+++ src/edu/uiowa/cs/warp/WorkLoadDescription.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package edu.uiowa.cs.warp;
+
+/**
+ * Reads the input file, whose name is passed as input parameter to the constructor, and builds a
+ * Description object based on the contents. Each line of the file is an entry (string) in the
+ * Description object.
+ * 
+ * @author sgoddard
+ * @version 1.4 Fall 2022
+ */
+public class WorkLoadDescription extends VisualizationObject {
+
+  private static final String EMPTY = "";
+  private static final String INPUT_FILE_SUFFIX = ".wld";
+
+  private Description description;
+  private String inputGraphString;
+  private FileManager fm;
+  private String inputFileName;
+
+  WorkLoadDescription(String inputFileName) {
+    super(new FileManager(), EMPTY, INPUT_FILE_SUFFIX); // VisualizationObject constructor
+    this.fm = this.getFileManager();
+    initialize(inputFileName);
+  }
+
+  @Override
+  public Description visualization() {
+    return description;
+  }
+
+  @Override
+  public Description fileVisualization() {
+    return description;
+  }
+
+  // @Override
+  // public Description displayVisualization() {
+  // return description;
+  // }
+
+  @Override
+  public String toString() {
+    return inputGraphString;
+  }
+
+  public String getInputFileName() {
+    return inputFileName;
+  }
+
+  private void initialize(String inputFile) {
+    // Get the input graph file name and read its contents
+    InputGraphFile gf = new InputGraphFile(fm);
+    inputGraphString = gf.readGraphFile(inputFile);
+    this.inputFileName = gf.getGraphFileName();
+    description = new Description(inputGraphString);
+  }
+}
