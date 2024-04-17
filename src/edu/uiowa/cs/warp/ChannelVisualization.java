aa16b5134ccbc53e5671daccc4a37d109c4ff2c3
mjmyhre <matthew-myhre@uiowa.edu>
2024-04-17 12:54:04 -0500

    adding channel visualization

--- src/edu/uiowa/cs/warp/ChannelVisualization.java
+++ src/edu/uiowa/cs/warp/ChannelVisualization.java
@@ -17,10 +17,91 @@ public class ChannelVisualization extends VisualizationObject {
   private static final String OBJECT_NAME = "Channel Analysis";
   private WarpInterface warp;
   private ChannelAnalysis ca;
+  
+  
+  private ProgramSchedule sourceCode;
+  /**
+   * the warp program instance
+   */
+  private Program program;
+  /**
+   * boolean flag to see if deadlines were met
+   */
+  private Boolean deadlinesMet;
+  /**
+   * constructor for new instance of ProgramVisualization
+   * @param Warp interface that give the information needed for ProgramVisualization
+   */
+  private ProgramSchedule schedule;
+  
 
   ChannelVisualization(WarpInterface warp) {
     super(new FileManager(), warp, SOURCE_SUFFIX);
     this.warp = warp;
     this.ca = warp.toChannelAnalysis();
+    this.deadlinesMet = warp.deadlinesMet();
+    this.program = warp.toProgram();
+    this.schedule = program.getSchedule();
+    
+    
+    
   }
+  @Override
+  public GuiVisualization displayVisualization() {
+      return new GuiVisualization(createTitle(), createColumnHeader(), createVisualizationData());
+      @Override
+  protected Description createHeader() {
+    	  Description header = new Description();
+    	  header.add(createTitle());
+          header.add(String.format("Scheduler Name: %s\n", program.getSchedulerName()));
+          // creating header
+          return header;
+      }
+      
+      
+   @Override
+   protected Description createFooter() {
+	    Description footer = new Description();
+	    String deadlineMsg = null;
+
+	    if (deadlinesMet) {
+	      deadlineMsg = "All flows meet their deadlines\n";
+	    } else {
+	      deadlineMsg = "WARNING: NOT all flows meet their deadlines. See deadline analysis report.\n";
+	    }
+	    footer.add(String.format("// %s", deadlineMsg));
+	    return footer;
+   
+   @Override
+   protected String[] createColumnHeader() {
+       // create header
+       return new String[]{"Time Slot", /* Other headers if needed */};
+   }
+
+   
+   
+   
+   @Override
+   protected String[][] createVisualizationData() {
+       // Generate visualization data from table in Channel analysis
+       if (visualizationData == null) {
+           int numRows = program.getNumChannels();
+       
+           int numColumns = schedule.getNumRows();
+           visualizationData = new String[numRows][numColumns + 1];
+
+           for (int row = 0; row < numRows; row++) {
+               visualizationData[row][0] = String.format("%s", row);
+               for (int column = 0; column < numColumns; column++) {
+                   visualizationData[row][column + 1] = schedule.get(row, column);
+               }
+           }
+       }
+       return visualizationData;
+   }
+
+   private String createTitle() {
+	   return String.format("Channel Analysis for Program: %s\n", program.getName());    
+   }
 }
+
