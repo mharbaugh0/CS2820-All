1e343e61bdadce7a142b4cda2d06a36e73dd01d4
Weipeng Wang <weipwang@uiowa.edu>
2024-04-18 17:26:18 -0500

    Create ChannelVisualizationTest.java

---
+++ test/edu/uiowa/cs/warp/ChannelVisualizationTest.java
@@ -0,0 +1,14 @@
+package edu.uiowa.cs.warp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChannelVisualizationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
