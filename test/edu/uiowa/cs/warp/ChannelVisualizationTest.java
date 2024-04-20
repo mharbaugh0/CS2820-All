ecd1eb7dbca647b309967c973dbb6397f7131302
mharbaugh <madeline-harbaugh@uiowa.edu>
2024-04-19 21:15:48 -0500

    ChannelVisualizationTest.java v2

--- test/edu/uiowa/cs/warp/ChannelVisualizationTest.java
+++ test/edu/uiowa/cs/warp/ChannelVisualizationTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.Test;
 class ChannelVisualizationTest {
     private ChannelVisualization channelVisualization;
     private WarpInterface warp;
-
     
     @Before
     public void setUp() {
@@ -23,44 +22,63 @@ class ChannelVisualizationTest {
     
 	@Test
 	@Timeout(value = 2000, unit = TimeUnit.MILLISECONDS)
-	void testDisplayVisualization() {
+	public void testDisplayVisualization() {
 		assertNotNull(channelVisualization.displayVisualization());
 	}
 
 	@Test
 	@Timeout(value = 2000, unit = TimeUnit.MILLISECONDS)
-	void testCreateHeader() {
+	public void testCreateHeader() {
 		assertNotNull(channelVisualization.createHeader());
 	}
 	
+    @Test
+    public void testCreateHeaderSchedulerName() {
+        var header = channelVisualization.createHeader();
+        assertTrue(header.toString().contains("Scheduler Name:"));
+    }
+	
 	@Test
 	@Timeout(value = 2000, unit = TimeUnit.MILLISECONDS)
-	void testCreateHeaderAndFooter() {
+	public void testCreateHeaderAndFooter() {
 		assertNotSame(channelVisualization.createHeader(),channelVisualization.createFooter());
 	}
 
 	@Test
 	@Timeout(value = 2000, unit = TimeUnit.MILLISECONDS)
-	void testCreateFooter() {
+	public void testCreateFooter() {
 		assertNotNull(channelVisualization.createFooter());
 	}
+	
+    @Test
+    public void testCreateFooterMessage() {
+        Description footer = channelVisualization.createFooter();
+        assertTrue(footer.toString().contains("All flows meet their deadlines") || 
+                   footer.toString().contains("WARNING: NOT all flows meet their deadlines"));
+    }
 
 	@Test
 	@Timeout(value = 2000, unit = TimeUnit.MILLISECONDS)
-	void testCreateColumnHeader() {
+	public void testCreateColumnHeader() {
 		assertNotNull(channelVisualization.createColumnHeader());
 	}
 
 	@Test
 	@Timeout(value = 2000, unit = TimeUnit.MILLISECONDS)
-	void testCreateVisualizationData() {
+	public void testCreateVisualizationData() {
 		assertNotNull(channelVisualization.createVisualizationData());
 	}
 
 	@Test
 	@Timeout(value = 2000, unit = TimeUnit.MILLISECONDS)
-	void testCreateTitle() {
+	public void testCreateTitle() {
 		assertNotNull(channelVisualization.createTitle());
 	}
-
+	
+    @Test
+    public void testCreateTitle2() {
+        String title = channelVisualization.createTitle();
+        assertTrue(title.contains("Channel Analysis"));
+    }
 }
+
