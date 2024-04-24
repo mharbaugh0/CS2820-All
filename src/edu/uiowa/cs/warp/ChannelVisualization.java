8b5e0ca6021f9f0b855d99d0dbd168536de972b6
martinezflores <martinezflores@uiowa.edu>
2024-04-24 13:13:27 -0500

    Semi completed Javadoc

--- src/edu/uiowa/cs/warp/ChannelVisualization.java
+++ src/edu/uiowa/cs/warp/ChannelVisualization.java
@@ -1,11 +1,12 @@
 package edu.uiowa.cs.warp;
 
 /**
- * ChannelVisualization creates the visualizations for the channel analysis of the WARP program.
+ * ChannelVisualization creates the visualizations for the channel analysis of
+ * the WARP program.
  * <p>
  * 
- * CS2820 Spring 2024 Project: Implement this class to create the file visualization that is requested
- * in Warp.
+ * CS2820 Spring 2024 Project: Implement this class to create the file
+ * visualization that is requested in Warp.
  * 
  * @author sgoddard
  * @version 1.7
@@ -14,130 +15,139 @@ package edu.uiowa.cs.warp;
 
 public class ChannelVisualization extends VisualizationObject {
 
-  private static final String SOURCE_SUFFIX = ".ch";
-  private static final String OBJECT_NAME = "Channel Analysis";
-  private WarpInterface warp;
-  private ChannelAnalysis ca;
-  
- 
-  private ProgramSchedule sourceCode;
-  /**
-   * the warp program instance
-   */
-  private Program program;
-  /**
-   * boolean flag to see if deadlines were met
-   */
-  private Boolean deadlinesMet;
-  /**
-   * constructor for new instance of ProgramVisualization
-   * @param Warp interface that give the information needed for ProgramVisualization
-   */
-  private ProgramSchedule schedule;
-  
-/**
- * 
- * @param warp is a warp element from WarpInterfac
- */
-  ChannelVisualization(WarpInterface warp) {
-    super(new FileManager(), warp, SOURCE_SUFFIX);
-    this.warp = warp;
-    this.ca = warp.toChannelAnalysis();
-    this.deadlinesMet = warp.deadlinesMet();
-    this.program = warp.toProgram();
-    this.schedule = program.getSchedule();
-    
-    
- /**
-  * The method is used to call the value of other methods in this class creating an object which
-  * can be used to display a chart. 
-  * @return GuiVisualization that can be used to make a chart
-  */
-  }
-  @Override
-  public GuiVisualization displayVisualization() {
-      return new GuiVisualization(createTitle(), createColumnHeader(), createVisualizationData());
-  }
-      @Override
-      
- /**
-  * The following method is used to create a header for the chart. 
-  * @return createHeader is used to make the header for the chart
-  */
-  protected Description createHeader() {
-    	  Description header = new Description();
-    	  header.add(createTitle());
-          header.add(String.format("Scheduler Name: %s\n", program.getSchedulerName()));
-          // creating header
-          return header;
-      }
-/**
- * This method is used to make the footer for the chart displaying the data. 
- * @return createFooter creates the footer for the chart
- */
-   @Override
-   protected Description createFooter() {
-	    Description footer = new Description();
-	    String deadlineMsg = null;
+	private static final String SOURCE_SUFFIX = ".ch";
+	private static final String OBJECT_NAME = "Channel Analysis";
+	private WarpInterface warp;
+	private ChannelAnalysis ca;
 
-	    if (deadlinesMet) {
-	      deadlineMsg = "All flows meet their deadlines\n";
-	    } else {
-	      deadlineMsg = "WARNING: NOT all flows meet their deadlines. See deadline analysis report.\n";
-	    }
-	    footer.add(String.format("// %s", deadlineMsg));
-	    return footer;
-   }
-  /**
-   * the method creates the column header for the chart. It dynamically changes depending on the size.
-   * @return createColumnHeader create 
-   */
-   @Override
-   protected String[] createColumnHeader() {
-       
-       int numChannels = program.getNumChannels();
+	private ProgramSchedule sourceCode;
+	/**
+	 * the warp program instance
+	 */
+	private Program program;
+	/**
+	 * boolean flag to see if deadlines were met
+	 */
+	private Boolean deadlinesMet;
+	/**
+	 * constructor for new instance of ProgramVisualization
+	 * 
+	 * @param Warp interface that give the information needed for
+	 *             ProgramVisualization
+	 */
+	private ProgramSchedule schedule;
 
-       
-       String[] headers = new String[numChannels + 1 ];  
-       headers[0] = "Time Slot";
-       for (int i = 1; i <= numChannels; i++) {
-           headers[i] = "Channel " + i;
-       }
-       return headers;
-   }
+	/**
+	 * 
+	 * @param warp is a warp element from WarpInterfac
+	 */
+	ChannelVisualization(WarpInterface warp) {
+		super(new FileManager(), warp, SOURCE_SUFFIX);
+		this.warp = warp;
+		this.ca = warp.toChannelAnalysis();
+		this.deadlinesMet = warp.deadlinesMet();
+		this.program = warp.toProgram();
+		this.schedule = program.getSchedule();
+	}
 
-   
-   
-/**
- * This method deals with the data involved in the chart. It checks if the chart is empty or not.
- * @return createVisualizationData
- */
-   @Override
-   protected String[][] createVisualizationData() {
-       // Generate visualization data from table in Channel analysis
-       if (visualizationData == null) {
-           int numRows = program.getNumChannels();
-       
-           int numColumns = schedule.getNumRows();
-           visualizationData = new String[numRows][numColumns + 1];
-           
+	/**
+	 * The method is used to call the value of other methods in this class creating
+	 * an object which can be used to display a chart.
+	 * 
+	 * @return GuiVisualization that can be used to make a chart
+	 */
 
-           for (int row = 0; row < numRows; row++) {
-               visualizationData[row][0] = String.format("%s", row);
-               for (int column = 0; column < numColumns; column++) {
-                   visualizationData[row][column + 1] = schedule.get(row, column);
-                
-               }
-           }
-       }
-       return visualizationData;
-   }
-/**
- * This method creates the title depending on the file name. 
- * @return createTitle takes the name of the file 
- */
-   private String createTitle() {
-	   return String.format("Channel Analysis for graph: %s\n", program.getName());    
-   }
+	@Override
+	public GuiVisualization displayVisualization() {
+		return new GuiVisualization(createTitle(), createColumnHeader(), createVisualizationData());
+	}
+
+	@Override
+
+	/**
+	 * The following method is used to create a header for the chart.
+	 * 
+	 * @return createHeader is used to make the header for the chart
+	 */
+	protected Description createHeader() {
+		Description header = new Description();
+		header.add(createTitle());
+		header.add(String.format("Scheduler Name: %s\n", program.getSchedulerName()));
+		// creating header
+		return header;
+	}
+
+	/**
+	 * This method is used to make the footer for the chart displaying the data.
+	 * 
+	 * @return createFooter creates the footer for the chart
+	 */
+	@Override
+	protected Description createFooter() {
+		Description footer = new Description();
+		String deadlineMsg = null;
+
+		if (deadlinesMet) {
+			deadlineMsg = "All flows meet their deadlines\n";
+		} else {
+			deadlineMsg = "WARNING: NOT all flows meet their deadlines. See deadline analysis report.\n";
+		}
+		footer.add(String.format("// %s", deadlineMsg));
+		return footer;
+	}
+
+	/**
+	 * the method creates the column header for the chart. It dynamically changes
+	 * depending on the size.
+	 * 
+	 * @return createColumnHeader create
+	 */
+	@Override
+	protected String[] createColumnHeader() {
+
+		int numChannels = program.getNumChannels();
+
+		// ADD NUMFAULTS
+		String[] headers = new String[numChannels + 1];
+		headers[0] = "Time Slot";
+		for (int i = 1; i <= numChannels; i++) {
+			headers[i] = "Channel " + i;
+		}
+		return headers;
+	}
+
+	/**
+	 * This method deals with the data involved in the chart. It checks if the chart
+	 * is empty or not.
+	 * 
+	 * @return createVisualizationData
+	 */
+	@Override
+	protected String[][] createVisualizationData() {
+		// Generate visualization data from table in Channel analysis
+		if (visualizationData == null) {
+			int numRows = program.getNumChannels();
+
+			int numColumns = schedule.getNumRows();
+			visualizationData = new String[numRows][numColumns + 1];
+
+			for (int row = 0; row < numRows; row++) {
+				visualizationData[row][0] = String.format("%s", row);
+				for (int column = 0; column < numColumns; column++) {
+					visualizationData[row][column + 1] = schedule.get(row, column);
+
+				}
+			}
+		}
+		return visualizationData;
+	}
+
+	/**
+	 * This method creates the title depending on the file name.
+	 * 
+	 * @return createTitle takes the name of the file
+	 */
+	String createTitle() {
+		return String.format("Channel Analysis for graph: %s\n", program.getName());
+	}
 }
-
